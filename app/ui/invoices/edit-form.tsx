2c3d41940a410562f43ae4102e3e80f8117a0d79
Asterios Raptis <asteri.raptis@gmail.com>
2024-11-07 17:12:19 +0100

    finish of chapter 15 auth

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -10,7 +10,8 @@ import {
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 
-import { updateInvoice } from '@/app/lib/actions';
+import { updateInvoice, State } from '@/app/lib/actions';
+import { useActionState } from 'react';
 
 export default function EditInvoiceForm({
   invoice,
@@ -20,7 +21,9 @@ export default function EditInvoiceForm({
   customers: CustomerField[];
 }) {
 
+  const initialState: State = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
   return (
     // Passing an id as argument won't work
     <form action={updateInvoiceWithId}>
